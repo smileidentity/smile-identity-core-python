e09c4b47fc7c1d88fd23a8fb413296a1b7001ef4
Japhet Ndhlovu <jndhlovu54@gmail.com>
2021-02-24 14:52:19 +0200

    black code formatting

--- smile_id_core/image_upload.py
+++ smile_id_core/image_upload.py
@@ -38,7 +38,9 @@ def generate_zip_file(
         zip_file.writestr("info.json", data=json.dumps(info_json))
         for image in image_params:
             image_file_path = image["image"]
-            if image_file_path.lower().endswith(IMAGE_FILE_EXTENSIONS):
+            if isinstance(image_file_path, str) and image_file_path.lower().endswith(
+                IMAGE_FILE_EXTENSIONS
+            ):
                 # TODO: do we really silently skip a file if its extension is different?
                 zip_file.write(image_file_path, os.path.basename(image_file_path))
     return zip_buffer.getvalue()
@@ -95,7 +97,9 @@ def prepare_image_payload(image_params):
 
 
 def prepare_image_entry_dict(image, image_type_id, **_):
-    if image.lower().endswith(IMAGE_FILE_EXTENSIONS):
+    if isinstance(image, bytes):
+        image = image.decode("utf-8")
+    elif image.lower().endswith(IMAGE_FILE_EXTENSIONS):
         return {
             "image_type_id": image_type_id,
             "image": "",
@@ -116,7 +120,9 @@ def validate_images(images_params):
         raise ValueError("Please ensure that you send through image details as a list")
 
     for image in images_params:
-        if image["image"].lower().endswith(IMAGE_FILE_EXTENSIONS):
+        if isinstance(image["image"], str) and image["image"].lower().endswith(
+            IMAGE_FILE_EXTENSIONS
+        ):
             if not os.path.exists(image["image"]):
                 raise FileNotFoundError(
                     "No such file or directory %s" % (image["image"])
