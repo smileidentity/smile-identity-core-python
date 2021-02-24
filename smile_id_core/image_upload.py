a07e7899333da46225851f8be3845cf746a7600d
Japhet Ndhlovu <jndhlovu54@gmail.com>
2021-02-24 14:45:38 +0200

    black code formatting

--- smile_id_core/image_upload.py
+++ smile_id_core/image_upload.py
@@ -14,14 +14,14 @@ IMAGE_FILE_EXTENSIONS = (".png", ".jpg")
 
 
 def generate_zip_file(
-        partner_id,
-        callback_url,
-        upload_url,
-        partner_params,
-        image_params,
-        id_info_params,
-        sec_key,
-        timestamp,
+    partner_id,
+    callback_url,
+    upload_url,
+    partner_params,
+    image_params,
+    id_info_params,
+    sec_key,
+    timestamp,
 ):
     info_json = prepare_info_json(
         partner_id,
@@ -38,21 +38,21 @@ def generate_zip_file(
         zip_file.writestr("info.json", data=json.dumps(info_json))
         for image in image_params:
             image_file_path = image["image"]
-            if isinstance(image_file_path, str) and image_file_path.lower().endswith(IMAGE_FILE_EXTENSIONS):
+            if image_file_path.lower().endswith(IMAGE_FILE_EXTENSIONS):
                 # TODO: do we really silently skip a file if its extension is different?
                 zip_file.write(image_file_path, os.path.basename(image_file_path))
     return zip_buffer.getvalue()
 
 
 def prepare_info_json(
-        partner_id,
-        callback_url,
-        upload_url,
-        partner_params,
-        image_params,
-        id_info_params,
-        sec_key,
-        timestamp,
+    partner_id,
+    callback_url,
+    upload_url,
+    partner_params,
+    image_params,
+    id_info_params,
+    sec_key,
+    timestamp,
 ):
     return {
         "package_information": {
@@ -95,9 +95,7 @@ def prepare_image_payload(image_params):
 
 
 def prepare_image_entry_dict(image, image_type_id, **_):
-    if isinstance(image, bytes):
-        image = image.decode("utf-8")
-    elif image.lower().endswith(IMAGE_FILE_EXTENSIONS):
+    if image.lower().endswith(IMAGE_FILE_EXTENSIONS):
         return {
             "image_type_id": image_type_id,
             "image": "",
@@ -118,7 +116,7 @@ def validate_images(images_params):
         raise ValueError("Please ensure that you send through image details as a list")
 
     for image in images_params:
-        if isinstance(image["image"], str) and image["image"].lower().endswith(IMAGE_FILE_EXTENSIONS):
+        if image["image"].lower().endswith(IMAGE_FILE_EXTENSIONS):
             if not os.path.exists(image["image"]):
                 raise FileNotFoundError(
                     "No such file or directory %s" % (image["image"])
