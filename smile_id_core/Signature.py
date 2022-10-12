2d23e6b6faab4233d1de8a99e428e9c39b04390d
mldangelo <michael.l.dangelo@gmail.com>
2022-10-11 22:27:50 -0400

    lint and document signature class

--- smile_id_core/Signature.py
+++ smile_id_core/Signature.py
@@ -1,31 +1,77 @@
+"""Defines signature class for Smile Identity Core API."""
 import base64
 import hashlib
 import hmac
+import sys
+import typing
 from datetime import datetime
 
 __all__ = ["Signature"]
 
+SignatureType = (
+    typing.TypedDict("SignatureType", {"signature": str, "timestamp": str})
+    if sys.version_info >= (3, 8)
+    else typing.Dict[str, str]
+)
+
 
 class Signature:
-    def __init__(self, partner_id: str, api_key: str):
-        if not partner_id or not api_key:
-            raise ValueError("partner_id or api_key cannot be null or empty")
-        self.partner_id = partner_id
-        self.api_key = api_key
+    """Class for generating signature for Smile Identity Core API."""
 
-    def generate_signature(self, timestamp=None):
-        _timestamp = timestamp
-        if _timestamp is None:
-            _timestamp = datetime.now().isoformat()
-        hmac_new = hmac.new(self.api_key.encode("utf-8"), digestmod=hashlib.sha256)
-        hmac_new.update(_timestamp.encode("utf-8"))
+    def __init__(self, partner_id: str, api_key: str):
+        """Initialize signature class.
+
+        Args:
+            partner_id (str): Smile Identity partner id.
+                Usually a 3 or 4 digit number. May contain leading zeros.
+            api_key (str): Smile Identity API key.
+                Please visit https://portal.smileidentity.com/api-key to get your API key.
+
+        Raises:
+            ValueError: _description_
+        """
+        if not isinstance(partner_id, str):
+            raise ValueError("partner_id must a string.")
+        if not isinstance(api_key, str):
+            raise ValueError("api_key must be a string.")
+
+        self.partner_id: str = partner_id
+        self.api_key: bytes = api_key.encode("utf-8")
+
+    def generate_signature(
+        self, timestamp: typing.Optional[str] = None
+    ) -> SignatureType:
+        """Generate signature for Smile Identity Core API.
+
+        Args:
+            timestamp (str, optional): Timestamp in ISO 8601 format. Defaults to None.
+
+        Returns:
+            SignatureType: Dictionary with signature and timestamp keys with string values.
+        """
+        if not isinstance(timestamp, str):
+            timestamp = datetime.now().isoformat()
+        hmac_new: hmac.HMAC = hmac.new(
+            self.api_key.encode("utf-8"),
+            digestmod=hashlib.sha256,
+        )
+        hmac_new.update(timestamp.encode("utf-8"))
         hmac_new.update(str(self.partner_id).encode("utf-8"))
         hmac_new.update(b"sid_request")
-        calculated_signature = base64.b64encode(hmac_new.digest())
+        calculated_signature: bytes = base64.b64encode(hmac_new.digest())
         return {
             "signature": calculated_signature.decode("utf-8"),
-            "timestamp": _timestamp,
+            "timestamp": timestamp,
         }
 
-    def confirm_signature(self, timestamp, msg_signature):
+    def confirm_signature(self, timestamp: str, msg_signature: str) -> bool:
+        """Confirm signature for Smile Identity Core API.
+
+        Args:
+            timestamp (str): Timestamp in ISO 8601 format.
+            msg_signature (str): Signature
+
+        Returns:
+            bool: True if signature is valid, False otherwise.
+        """
         return self.generate_signature(timestamp)["signature"] == msg_signature
