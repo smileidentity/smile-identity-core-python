3126090336acefd35979be5d0005fbe7a173d63f
Ralph Blanes <rblanes31@gmail.com>
2022-10-18 23:23:07 +0000

    Removes version definition from setup.py

--- smile_id_core/__init__.py
+++ smile_id_core/__init__.py
@@ -1,7 +1,10 @@
+# from ._version import __version__
 from smile_id_core.Utilities import Utilities
 from smile_id_core.IdApi import IdApi
 from smile_id_core.WebApi import WebApi
 from smile_id_core.Signature import Signature
 from smile_id_core.ServerError import ServerError
 
+__version__ = "1.0.8"
 __all__ = ["IdApi", "Signature", "Utilities", "WebApi", "ServerError"]
+
