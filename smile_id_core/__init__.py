9c302545e13dff4f40be53df1163b70f7ea5ccf5
Vansh Gandhi <vansh@smileidentity.com>
2022-11-01 07:44:21 -0700

    Pick version dynamically and update lock file

--- smile_id_core/__init__.py
+++ smile_id_core/__init__.py
@@ -1,8 +1,10 @@
-from smile_id_core.Utilities import Utilities
-from smile_id_core.IdApi import IdApi
-from smile_id_core.WebApi import WebApi
-from smile_id_core.Signature import Signature
-from smile_id_core.ServerError import ServerError
+import importlib.metadata
 
-__version__ = "1.0.8"
+from smile_id_core.IdApi import IdApi
+from smile_id_core.ServerError import ServerError
+from smile_id_core.Signature import Signature
+from smile_id_core.Utilities import Utilities
+from smile_id_core.WebApi import WebApi
+
+__version__ = importlib.metadata.version(__package__)
 __all__ = ["IdApi", "Signature", "Utilities", "WebApi", "ServerError"]
