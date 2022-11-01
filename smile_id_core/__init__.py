49651d2704d25a66e8d3bec8996ee0ed6051d896
Vansh Gandhi <vansh@smileidentity.com>
2022-11-01 07:55:13 -0700

    Python 3.7 compatibility

--- smile_id_core/__init__.py
+++ smile_id_core/__init__.py
@@ -1,4 +1,8 @@
-import importlib.metadata
+# import importlib.metadata if available, otherwise importlib_metadata (for Python < 3.8)
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
 
 from smile_id_core.IdApi import IdApi
 from smile_id_core.ServerError import ServerError
@@ -6,5 +10,5 @@ from smile_id_core.Signature import Signature
 from smile_id_core.Utilities import Utilities
 from smile_id_core.WebApi import WebApi
 
-__version__ = importlib.metadata.version(__package__)
+__version__ = importlib_metadata.version(__package__)
 __all__ = ["IdApi", "Signature", "Utilities", "WebApi", "ServerError"]
