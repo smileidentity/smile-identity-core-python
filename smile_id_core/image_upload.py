0966c2c86780ec907d8ac615a68874c1e04d0743
AbigailWoolley <woolleyabigai47@gmail.com>
2023-03-01 12:22:58 +0000

    Fixed bug in image upload file and updated test file accordingly

--- smile_id_core/image_upload.py
+++ smile_id_core/image_upload.py
@@ -1,12 +1,24 @@
+"""Prepare & validate image data, and generate zipped file to be submitted."""
 import io
 import json
 import os
 import zipfile
+from typing import Any, ByteString, Dict, List, Optional, cast
 
+from smile_id_core import constants
+from smile_id_core.constants import JobType
+from smile_id_core.types import (
+    Base64Image,
+    FileImage,
+    ImageParams,
+    SignatureParams,
+)
 from smile_id_core.Utilities import validate_signature_params
 
 
 class ApiVersion:
+    """Specify API version details."""
+
     BUILD_NUMBER = 0
     MAJOR_VERSION = 2
     MINOR_VERSION = 0
@@ -16,14 +28,26 @@ IMAGE_FILE_EXTENSIONS = (".png", ".jpg", ".jpeg")
 
 
 def generate_zip_file(
-    partner_id,
-    callback_url,
-    upload_url,
-    partner_params,
-    image_params,
-    id_info_params,
-    signature_params,
-):
+    partner_id: str,
+    callback_url: str,
+    upload_url: str,
+    partner_params: Dict[str, Any],
+    image_params: List[ImageParams],
+    id_info_params: Dict[str, str],
+    signature_params: SignatureParams,
+) -> ByteString:
+    """Create zipped file with a number of various params.
+
+    argument(s):
+        partner_id
+        callback_url
+        upload_url
+        partner_params
+        image_params
+        id_info_params
+        signature params
+    Returns: zipped filed of ByteString type
+    """
     info_json = prepare_info_json(
         partner_id,
         callback_url,
@@ -39,26 +63,48 @@ def generate_zip_file(
     ) as zip_file:
         zip_file.writestr("info.json", data=json.dumps(info_json))
         for image in image_params:
-            image_file_path = image["image"]
-            if isinstance(
-                image_file_path, str
-            ) and image_file_path.lower().endswith(IMAGE_FILE_EXTENSIONS):
-                # TODO: do we really silently skip a file if its extension is different?
-                zip_file.write(
-                    image_file_path, os.path.basename(image_file_path)
+            if (
+                "image" in image
+                and image["image_type_id"] in constants.BASE64_IMAGE_TYPES
+            ):
+                image = cast(Base64Image, image)
+                zip_file.writestr(
+                    "base64imgString", os.path.basename(image["image"])
                 )
+            elif (
+                "file_name" in image
+                and image["image_type_id"] in constants.FILENAME_IMAGE_TYPES
+            ):
+                image = cast(FileImage, image)
+                if image["file_name"] is not None:
+                    zip_file.write(
+                        image["file_name"],
+                        os.path.basename(image["file_name"]),
+                    )
     return zip_buffer.getvalue()
 
 
 def prepare_info_json(
-    partner_id,
-    callback_url,
-    upload_url,
+    partner_id: str,
+    callback_url: str,
+    upload_url: str,
+    partner_params: Dict[str, str],
+    image_params: List[ImageParams],
+    id_info_params: Dict[str, str],
+    signature_params: SignatureParams,
+) -> Dict[str, Any]:
+    """Call validate_signature_params to validate signature params.
+
+    argument(s):
+    partner_id
+    callback_url
+    upload_url
     partner_params,
-    image_params,
-    id_info_params,
-    signature_params,
-):
+    image_params
+    id_info_params
+    signature params
+    Returns: Dict response
+    """
     validate_signature_params(signature_params)
     return {
         "package_information": {
@@ -95,14 +141,46 @@ def prepare_info_json(
     }
 
 
-def prepare_image_payload(image_params):
-    return [prepare_image_entry_dict(**image) for image in image_params]
+def prepare_image_payload(
+    image_params: List[ImageParams],
+) -> List[Dict[str, Any]]:
+    """Call the prepare_image_entry_dict on image params.
+
+    argument(s):
+        image_params: images details of type Dict of List. Contains image type
+        id, a path or base64 string to the image(id doc or selfie).
+    Returns: Dict of list return based on image format(base64 or image file)
+    """
+    image_lists = []
+    for image in image_params:
+        type_id = int(image["image_type_id"])
+        file = ""
+
+        if "image" in image.keys() and image.get("image") is not None:
+            file = str(image.get("image"))
+        if "file_name" in image.keys():
+            file = str(image.get("file_name"))
+        image_lists.append(prepare_image_entry_dict(file, type_id))
+    return image_lists
 
 
-def prepare_image_entry_dict(image, image_type_id, **_):
-    if isinstance(image, bytes):
-        image = image.decode("utf-8")
-    elif image.lower().endswith(IMAGE_FILE_EXTENSIONS):
+def prepare_image_entry_dict(
+    image: Optional[str], image_type_id: int
+) -> Dict[str, Any]:
+    """Check for image format and return image info as dict.
+
+    argument(s):
+        image: Image is a dictionary of a list and it contains image data info
+        .The value to the key could be a string specifying path to an image
+        file or a base64 image string.
+        image_type_id: This is an int value which specifies which kind of
+        image data is provided based on it's unique value.
+
+    Returns:
+    Returns an image data dictionary containing image_type_id, image, the
+    filename.
+    """
+    if image and image.lower().endswith(IMAGE_FILE_EXTENSIONS):
         return {
             "image_type_id": image_type_id,
             "image": "",
@@ -115,7 +193,20 @@ def prepare_image_entry_dict(image, image_type_id, **_):
     }
 
 
-def validate_images(images_params, use_enrolled_image=False, job_type=None):
+def validate_images(
+    images_params: List[ImageParams],
+    use_enrolled_image: bool = False,
+    job_type: Optional[JobType] = None,
+) -> bool:
+    """Perform validation checks on images_params.
+
+    argument(s):
+        image_params of type List
+        use_enrolled_imge: bool
+        job_type: specifies job type
+
+    Returns: A boolean value based on checks for jobtype 6
+    """
     if not images_params:
         raise ValueError("Please ensure that you send through image details")
 
@@ -127,24 +218,53 @@ def validate_images(images_params, use_enrolled_image=False, job_type=None):
     has_id_image = False
     has_selfie = False
     for image in images_params:
-        if isinstance(image["image"], str) and image["image"].lower().endswith(
-            IMAGE_FILE_EXTENSIONS
-        ):
-            if not os.path.exists(image["image"]):
+        image_file_name = image.get("file_name", None)
+        base64_image = image.get("image", None)
+        image_type = image.get("image_type_id", None)
+
+        if image_type is None:
+            raise ValueError("image_type_id cannot be None or empty")
+        if base64_image and image_file_name:
+            raise ValueError("image and file_name both can't exist")
+        if not base64_image and not image_file_name:
+            raise ValueError("image or file_name keys both can't be empty")
+        if base64_image and image_type in constants.FILENAME_IMAGE_TYPES:
+            raise ValueError("check for image_type_id and image file mismatch")
+        if image_file_name and image_type in constants.BASE64_IMAGE_TYPES:
+            raise ValueError(
+                "check for image_type_id and base64 image mismatch"
+            )
+        if image_file_name and isinstance(image_file_name, str):
+            if image_file_name.lower().endswith(
+                IMAGE_FILE_EXTENSIONS
+            ) and not os.path.exists(image_file_name):
                 raise FileNotFoundError(
-                    f"No such file or directory {image['image']}"
+                    f"No such file or directory {image_file_name}"
                 )
 
-        image_type_id = int(image["image_type_id"])
+        image_type_id = image["image_type_id"]
         if image_type_id == 1 or image_type_id == 3:
             has_id_image = True
         if image_type_id == 0 or image_type_id == 2:
             has_selfie = True
 
-    if job_type == 6 and not has_id_image:
+    if job_type == JobType.DOCUMENT_VERIFICATION and not has_id_image:
         raise ValueError(
-            "You are attempting to complete a job type 6 without providing an id card image."
+            "You are attempting to complete a Document Verification job "
+            "without providing an id card image."
         )
 
-    if not (has_selfie or (job_type == 6 and use_enrolled_image)):
+    if not (
+        has_selfie
+        or (job_type == JobType.DOCUMENT_VERIFICATION and use_enrolled_image)
+    ):
         raise ValueError("You need to send through at least one selfie image.")
+
+    if (
+        has_id_image
+        and has_selfie
+        and (job_type == JobType.DOCUMENT_VERIFICATION and use_enrolled_image)
+        and (job_type == JobType.DOCUMENT_VERIFICATION and not has_id_image)
+    ):
+        return False
+    return True
--- tests/test_image_upload.py
+++ tests/test_image_upload.py
@@ -1,10 +1,13 @@
+import base64
 import io
 import os
 import tempfile
 import zipfile
+from typing import List
 
 import pytest
 
+from smile_id_core.constants import JobType
 from smile_id_core.image_upload import (
     generate_zip_file,
     prepare_image_entry_dict,
@@ -12,9 +15,18 @@ from smile_id_core.image_upload import (
     prepare_info_json,
     validate_images,
 )
+from smile_id_core.types import ImageParams
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(current_dir, "../tests/fixtures/1pixel.jpg")
+
+with open(image_path, "rb") as binary_file:
+    base64_data = base64.b64encode(binary_file.read())
+    base64_img = base64_data.decode("utf-8")
 
 
 def test_prepare_image_entry_dict():
+    """Tests image entry dict"""
     assert prepare_image_entry_dict("directory/file.jpg", 5) == {
         "image_type_id": 5,
         "image": "",
@@ -26,12 +38,27 @@ def test_prepare_image_entry_dict():
         "file_name": "",
     }
 
+    with open(image_path, "rb") as image:
+        encoded_string = base64.b64encode(image.read())
+        image_file_path = base64.b64decode(encoded_string).decode("utf-8")
+
+    assert prepare_image_entry_dict(image_file_path, 5) == {
+        "image_type_id": 5,
+        "image": image_file_path,
+        "file_name": "",
+    }
+
+    assert (
+        prepare_image_entry_dict(image_file_path, 5)["image"] == image_file_path
+    )
+
 
 def test_prepare_info_json():
-    image_params = [
+    """Validates inputs to prepare_info_json function"""
+    image_params: List[ImageParams] = [
         {
-            "image": "directory/file.jpg",
             "image_type_id": 5,
+            "file_name": "directory/file.jpg",
         }
     ]
 
@@ -39,9 +66,9 @@ def test_prepare_info_json():
         partner_id="partner_id",
         callback_url="callback_url",
         upload_url="upload_url",
-        partner_params="partner_params",
+        partner_params="partner_params",  # type: ignore
         image_params=image_params,
-        id_info_params="id_info_params",
+        id_info_params="id_info_params",  # type: ignore
         signature_params={
             "signature": "signature",
             "timestamp": "timestamp",
@@ -54,6 +81,7 @@ def test_prepare_info_json():
 
 @pytest.fixture()
 def temp_image_file():
+    """Creates temp image file (Generator)"""
     with tempfile.NamedTemporaryFile(
         delete=False, suffix=".jpg"
     ) as temp_image_file:
@@ -64,14 +92,14 @@ def temp_image_file():
     os.remove(temp_image_file.name)
 
 
-def test_generate_zip_file(temp_image_file):
+def test_generate_zip_file() -> None:
+    """Generates zipped file with temp_image_file"""
+    assert image_path.endswith(".jpg")
 
-    assert temp_image_file.endswith(".jpg")
-
-    image_params = [
+    image_params: List[ImageParams] = [
         {
-            "image": temp_image_file,
             "image_type_id": 5,
+            "file_name": image_path,
         }
     ]
 
@@ -79,32 +107,37 @@ def test_generate_zip_file(temp_image_file):
         partner_id="partner_id",
         callback_url="callback_url",
         upload_url="upload_url",
-        partner_params="partner_params",
+        partner_params="partner_params",  # type: ignore
         image_params=image_params,
-        id_info_params="id_info_params",
+        id_info_params="id_info_params",  # type: ignore
         signature_params={"signature": "signature", "timestamp": "timestamp"},
     )
 
-    zf = zipfile.ZipFile(io.BytesIO(zip_stream))
-    assert zf.namelist() == ["info.json", os.path.basename(temp_image_file)]
+    with zipfile.ZipFile(io.BytesIO(bytes(zip_stream))) as zipped_file:
+        assert zipped_file.namelist() == [
+            "info.json",
+            os.path.basename(image_path),
+        ]
 
 
-def test_validate_images__ok_file_exists(temp_image_file):
-    image_params = [
+def test_validate_images__ok_file_exists() -> None:
+    """Validates image file exists (is Not None)"""
+    image_params: List[ImageParams] = [
         {
-            "image": temp_image_file,
             "image_type_id": 0,
+            "file_name": image_path,
         }
     ]
 
-    assert validate_images(image_params) is None
+    assert validate_images(image_params) is not None
 
 
 def test_validate_images__error_file_not_found():
-    image_params = [
+    """Validates that non-existent image files is not found"""
+    image_params: List[ImageParams] = [
         {
-            "image": "nonexistent/file.jpg",
             "image_type_id": 5,
+            "file_name": "nonexistent/file.jpg",
         }
     ]
 
@@ -115,63 +148,175 @@ def test_validate_images__error_file_not_found():
     )
 
 
-def test_validate_images_jt6_id_not_provided():
-    image_params = [
+def test_validate_images_jt6_id_not_provided() -> None:
+    """Tests for missing id card for document verification"""
+    image_params: List[ImageParams] = [
         {
-            "image": "wwsss===",
             "image_type_id": 0,
+            "file_name": image_path,
         }
     ]
 
     with pytest.raises(ValueError) as exc_info:
-        validate_images(image_params, job_type=6)
+        validate_images(image_params, job_type=JobType.DOCUMENT_VERIFICATION)
     assert (
         str(exc_info.value)
-        == "You are attempting to complete a job type 6 without providing an id card image."
+        == "You are attempting to complete a Document Verification job without"
+        " providing an id card image."
     )
 
 
-def test_validate_images_requires_at_least_one_selfie_for_jt_other_than_jt6():
-    image_params = [
+def test_validate_images_require_a_selfie_for_alljt_except_jt6() -> None:
+    """Checks that image validation for all job types other than
+    document verification have at least 1 selfie image"""
+    image_params: List[ImageParams] = [
         {
-            "image": "wwsss===",
             "image_type_id": 1,
+            "file_name": image_path,
         }
     ]
 
     with pytest.raises(ValueError) as exc_info:
-        validate_images(image_params, job_type=1, use_enrolled_image=False)
-    assert (
-        str(exc_info.value)
-        == "You need to send through at least one selfie image."
-    )
-
-
-def test_validate_images_requires_at_least_one_selfie_for_jt6_when_use_enrolled_image_is_false():
-    image_params = [
-        {
-            "image": "wwsss===",
-            "image_type_id": 1,
-        }
-    ]
-
-    with pytest.raises(ValueError) as exc_info:
-        validate_images(image_params, job_type=6, use_enrolled_image=False)
+        validate_images(
+            image_params,
+            job_type=JobType.BIOMETRIC_KYC,
+            use_enrolled_image=False,
+        )
     assert (
         str(exc_info.value)
         == "You need to send through at least one selfie image."
     )
 
 
-def test_validate_images_should_not_requires_at_least_one_selfie_for_jt6_when_use_enrolled_image_is_true():
-    image_params = [
+def test_validate_images_require_a_selfie_when_enrolled_image_is_false_jt6() -> None:
+    """Checks that attempted image validation for all job types other than
+    document verification require at least 1 selfie image with
+    use_enrolled_image set to false"""
+
+    image_params: List[ImageParams] = [
         {
-            "image": "wwsss===",
             "image_type_id": 1,
+            "file_name": image_path,
+        }
+    ]
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=False,
+        )
+    assert (
+        str(exc_info.value)
+        == "You need to send through at least one selfie image."
+    )
+
+
+def test_validate_images_require_no_selfie_when_enrolled_image_is_true_jt6() -> None:
+    image_params: List[ImageParams] = [
+        {
+            "image_type_id": 1,
+            "file_name": image_path,
         }
     ]
 
     assert (
-        validate_images(image_params, job_type=6, use_enrolled_image=True)
-        is None
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=True,
+        )
+        is True
+    )
+
+
+def test_validate_images_params():
+    image_params: List[ImageParams] = [
+        {
+            "image_type_id": 5,
+            "file_name": "",
+        }
+    ]
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=True,
+        )
+    assert str(exc_info.value) == "image or file_name keys both can't be empty"
+
+    image_params = [
+        {
+            "image": base64_img,
+            "image_type_id": 5,
+            "file_name": image_path,
+        }  # type: ignore
+    ]
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=True,
+        )
+    assert str(exc_info.value) == "image and file_name both can't exist"
+
+    image_params = [
+        {
+            "image_type_id": 3,
+            "file_name": image_path,
+        }
+    ]
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=True,
+        )
+    assert (
+        str(exc_info.value)
+        == "check for image_type_id and base64 image mismatch"
+    )
+
+    image_params = [
+        {
+            "image": base64_img,
+            "image_type_id": 1,
+        }
+    ]
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.DOCUMENT_VERIFICATION,
+            use_enrolled_image=True,
+        )
+    assert (
+        str(exc_info.value) == "check for image_type_id and image file mismatch"
+    )
+
+    image_params: List[ImageParams] = [{"image": base64_img, "image_type_id": None}]  # type: ignore
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,
+            job_type=JobType.BIOMETRIC_KYC,
+            use_enrolled_image=True,
+        )
+    assert str(exc_info.value) == "image_type_id cannot be None or empty"
+
+
+def test_validate_image_is_a_list():
+    image_params = {"image": base64_img, "image_type_id": 4}
+    with pytest.raises(ValueError) as exc_info:
+        validate_images(
+            image_params,  # type: ignore
+            job_type=JobType.BIOMETRIC_KYC,
+            use_enrolled_image=True,
+        )
+    assert (
+        str(exc_info.value)
+        == "Please ensure that you send through image details as a list"
     )
